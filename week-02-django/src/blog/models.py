30c9c875f00af61263055e43e8a865c06177f147
정희윤 <ChungHeeyoon@jeonghuiyuns-MacBook-Pro.local>
2017-01-05 01:09:38 +0900

    Utilizing Bootstrap

--- week-02-django/src/blog/models.py
+++ week-02-django/src/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+#아티클 이라는 클래스를 만드는데 import 한 models 에서 model이라는 클래스를 가져온것.
+class Article(models.Model):
+    title = models.CharField(max_length=30) #charfield 는 항상 맥스 길이가 있어야 한다.
+    contents = models.TextField()
+    view_count= models.IntegerField()
+
+    def __str__(self): #클래스 안에 함수 만들때 이름에 언더바 두개 넣는 경우 많다. 그리고 꼭 self해줘야함
+        return "{} ({})".format(self.title, self.view_count)
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article)
+    comment = models.CharField(max_length=100)
--- week-02-django/src/blog/views.py
+++ week-02-django/src/blog/views.py
@@ -1,7 +1,22 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
-
+# from random import randint
 # Create your views here.
+from .models import Article
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    # random_number= randint(1,10)
+    # return HttpResponse("Hello, world. {}".format(random_number))
+    # return HttpResponse("Hello, world. You're at the index.")
+    # name = "marco"
+    # return render(request,"index.html",{"name":name})
+    article_list = Article.objects.all() #Load everything from Article
+    # Article.objects.create(
+    #     title="hello",
+    #     contents="thisistest",
+    #     view_count=0
+    # )
+    ctx={
+        "article_list" : article_list
+    } #created a dictionary called ctx(context)
+    return render(request,"index.html",ctx)
